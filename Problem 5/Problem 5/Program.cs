caf1d12abdac3db3c7f51a2cbe759d28b27d4cfd
James Alaniz <jalaniz1@mail.csuchico.edu>
2018-06-09 21:09:57 -0700

    Completed problem 4. Just needed to consider all coefficients until the edge of the numbers. Completed problem 5 as well.

---
+++ Problem 5/Problem 5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Problem_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            Console.WriteLine(GetSmallestDivisibleNbrByRange(1, 20));
+            Console.ReadLine();
+        }
+
+        static bool IsNbrNotEvenlyDivisibleBy(int nbr, int divisor)
+        {
+            return nbr % divisor != 0;
+        }
+
+        static int GetSmallestDivisibleNbrByRange(int start, int max)
+        {
+
+            int cnt = start, curNbr = 1;
+            
+            while (cnt <= max)
+            {
+                 
+                if(IsNbrNotEvenlyDivisibleBy(curNbr, cnt))
+                {
+                    cnt = start;
+                    curNbr++;
+                    continue;
+                }
+
+                cnt++;
+            }
+
+            return curNbr;
+        }
+    }
+}
